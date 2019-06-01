0fbfb1007cf0c15204283055c4a8e176af2146d0
Dalan Lewis <dalanforpresident@gmail.com>
2019-06-01 11:21:48 -0600

    put is now working

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('monk')('mongodb://gamemaster:abcxyz123@ds151076.mlab.com:51076/gameifydb')
+const userData = db.get('users')
+const app = express()
+const port = 8888
+
+
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+})
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const user = await userData.find(req.params._id)
+        res.send(user)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        await userData.insert(req.body)
+        res.send()
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.put('/users/:_id', async (req, res) => {
+    try {
+        console.log(req.body)
+        const stuff = await userData.findOneAndUpdate(req.params._id, req.body)
+        res.send(stuff)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.delete('/:id', (req, res) => res.send('D-e-l-eted'))
+
+app.listen(port, () => console.log("i'm listening!"))
